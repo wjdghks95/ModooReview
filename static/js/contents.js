6f7d83eb8e9f5e16a9e6da4725c6b4c99b53816f
wjdghks95 <wjdghks2799@Naver.com>
2022-12-01 17:54:20 +0900

    Contents 카테고리 슬라이드

--- static/js/contents.js
+++ static/js/contents.js
@@ -7,3 +7,28 @@ $('.content__view').on('click', function() {
     $('.view-btn').removeClass('active');
     $('.view-btn').eq(index).addClass('active');
 })
+
+// 카테고리 슬라이드
+let currentIdx = 0;
+let slideCount = $('.slide').length;
+
+function moveSlide(num) {
+    $('.slides').css('left', -num * 120 + 'px');
+    currentIdx = num;
+}
+
+$('.arrow-btn--next').on('click', function() {
+    if(currentIdx < slideCount - 11) {
+        moveSlide(currentIdx + 1);
+    } else {
+        return;
+    }
+})
+
+$('.arrow-btn--prev').on('click', function() {
+    if(currentIdx > 0) {
+        moveSlide(currentIdx - 1);
+    } else {
+        return;
+    }
+})
