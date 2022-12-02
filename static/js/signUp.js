7e37b18e484d71aac20d680b7b079bc90466dee6
wjdghks95 <wjdghks2799@Naver.com>
2022-12-02 18:39:11 +0900

    SignUp 휴대폰 인증

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -1,3 +1,54 @@
+// 휴대폰 인증
+let code = "";
+let isPhoneChk = false;
+
+$('.sign-up-modal__sms-button').on('click', function() {
+    const phoneNum = $('#user-phone').val();
+
+    if (phoneValidator(phoneNum)) {
+        alert("인증번호가 발송되었습니다.");
+
+        $(this).children().attr('disabled', true);
+        $('#user-phone').attr('readonly', true);
+        $('.sign-up-modal__phoneChk').show();
+
+        code = "0000";
+    } else {
+        alert("유효하지 않는 전화번호입니다.");
+    }
+})
+
+function phoneValidator(phoneNum) {
+    let patternPhone = new RegExp("01[016789][^0][0-9]{2,3}[0-9]{4}$");
+    if(!patternPhone.test(phoneNum)) {
+        return false;
+    }
+    return true;
+}
+
+$('.sign-up-modal__phoneChk-button').on('click', function() {
+    const phoneChkNum = $('#user-phoneChk').val();
+    phoneChk(phoneChkNum);
+})
+
+function phoneChk(phoneChkNum) {
+    if (phoneChkNum === code) {
+        alert("인증이 완료되었습니다.");
+
+        $('#user-phoneChk').attr("disabled", true);
+        $('.sign-up-modal__resend-sms-button').children().attr("disabled", true);
+        isPhoneChk = true;
+    } else {
+        alert("인증번호가 올바르지 않습니다. 다시 확인해 주세요.");
+        $('#user-phoneChk').focus();
+        isPhoneChk = false;
+    }
+}
+
+$('.sign-up-modal__resend-sms-button').on('click', function() {
+    $('.sign-up-modal__sms-button').click();
+})
+
 // 이메일 주소 선택
 $('#user-email, #user-email-addr').on('blur', function() {
     addUpEmail();
@@ -34,6 +85,11 @@ $('#user-password, #user-passwordChk').on('keyup', function() {
 
 // Submit
 $('.sign-up-modal__submit-button').on('click', function() {
+    if(!isPhoneChk) {
+        alert('휴대폰 인증을 완료해주세요.');
+        return;
+    }
+
     if(!isPwdChk) { // 패스워드 일치 확인
         alert('비밀번호를 확인해주세요.');
         return;
