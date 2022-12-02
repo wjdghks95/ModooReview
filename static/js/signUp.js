780874961bf36168b5aa03957d0d3abc1114aaee
wjdghks95 <wjdghks2799@Naver.com>
2022-12-02 17:16:57 +0900

    SignUp 비밀번호 확인

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -18,3 +18,26 @@ function addUpEmail() {
         $('#user-total-email').val($('#user-email').val() + "@" + $('#user-email-addr').val());
     }
 };
+
+// 비밀번호 확인
+let isPwdChk = false;
+
+$('#user-password, #user-passwordChk').on('keyup', function() {
+    if($('#user-password').val() !== $('#user-passwordChk').val()) {
+        $('.sign-up-modal__pwdChk-error-msg').text('비밀번호가 일치하지 않습니다').addClass('error-msg');
+        isPwdChk = false;
+    } else {
+        $('.sign-up-modal__pwdChk-error-msg').text('비밀번호가 일치합니다.').removeClass('error-msg');
+        isPwdChk = true;
+    }
+})
+
+// Submit
+$('.sign-up-modal__submit-button').on('click', function() {
+    if(!isPwdChk) { // 패스워드 일치 확인
+        alert('비밀번호를 확인해주세요.');
+        return;
+    }
+
+    $('.sign-up-modal__form').submit();
+})
