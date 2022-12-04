053458bd5ba4b92f1c6af382a647739e3594a8f8
wjdghks95 <wjdghks2799@Naver.com>
2022-12-04 10:10:49 +0900

    Fragment 분리

--- rol/src/main/java/com/io/rol/TestController.java
+++ rol/src/main/java/com/io/rol/TestController.java
@@ -2,14 +2,12 @@ package com.io.rol;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
     @GetMapping("/")
-    @ResponseBody
     public String test() {
-        return "test";
+        return "index";
     }
 }
