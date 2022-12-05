15a2743f921501514c8699a5f22fad721d21a8b0
wjdghks95 <wjdghks2799@Naver.com>
2022-12-05 20:18:56 +0900

    JpaAuditing - BaseTime

--- rol/src/main/java/com/io/rol/domain/entity/Member.java
+++ rol/src/main/java/com/io/rol/domain/entity/Member.java
@@ -1,5 +1,6 @@
 package com.io.rol.domain.entity;
 
+import com.io.rol.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
