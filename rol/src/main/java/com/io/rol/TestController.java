b5ab3e58c879f4a5ed86b669fbd1bf849f8c1a95
wjdghks95 <wjdghks2799@Naver.com>
2022-12-06 20:21:30 +0900

    build 및 서버, db 테스트

---
+++ rol/src/main/java/com/io/rol/TestController.java
@@ -0,0 +1,15 @@
+package com.io.rol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
---
+++ rol/src/test/java/com/io/rol/RolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.rol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
